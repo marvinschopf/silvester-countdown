5b5a76c78ea35575522d0421f4c7e65f332c1ccb
Marvin Schopf <marvin@schopf.biz>
2021-02-23 18:42:16 +0100

    remove border

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ export default class App extends Component {
             <div style={{ 
                 backgroundImage: `url("https://via.placeholder.com/500")`,
                 height: "100%",
-                width: "100%"
+                width: "100%",
+                margin: 0,
+                border: "none"
             }}>
                 <h1>Hi</h1>
             </div>
