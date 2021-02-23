0c3ed40e859177c16a7d6b8c4b5ab992d12aae4a
Marvin Schopf <marvin@schopf.biz>
2021-02-23 18:38:21 +0100

    make main div full width

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ export default class App extends Component {
     render() {
         return (
             <div style={{ 
-                backgroundImage: `url("https://via.placeholder.com/500")` 
+                backgroundImage: `url("https://via.placeholder.com/500")`,
+                height: "100%",
+                width: "100%"
             }}>
                 <h1>Hi</h1>
             </div>
