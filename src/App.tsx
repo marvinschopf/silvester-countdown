e925d86748864ed187a07d1b94e3bfee108af07c
Marvin Schopf <marvin@schopf.biz>
2021-02-23 17:41:10 +0100

    cleanup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Component, render, h } from 'preact';
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
